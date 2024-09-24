cf4c0b67568b7206aeff368d6ef5b486ec787d12
Ash-Shiddiqul Akbar Hidayat <diqo@dicoding.com>
2024-09-24 14:05:27 +0700

    added rule to detect possibility to exploit inclusion via php://filters

---
+++ php/php-filter-inclusion.php
@@ -0,0 +1,55 @@
+<?php
+
+// ok: php-filter-inclusion
+include "a";
+// ok: php-filter-inclusion
+require "b";
+// ok: php-filter-inclusion
+include_once "a";
+// ok: php-filter-inclusion
+require_once "b";
+
+class SebuahClass {
+    public function load($url) {
+        // ruleid: php-filter-inclusion
+        include $url;
+        // ruleid: php-filter-inclusion
+        require $url;
+        // ruleid: php-filter-inclusion
+        include_once $url;
+        // ruleid: php-filter-inclusion
+        require_once $url;
+
+        // ruleid: php-filter-inclusion
+        include "a.php";
+        // ruleid: php-filter-inclusion
+        require "a.php";
+        // ruleid: php-filter-inclusion
+        include_once "a.php";
+        // ruleid: php-filter-inclusion
+        require_once "a.php";
+    }
+
+    public function loadViaIncludeFunction($url) {
+        // ruleid: php-filter-inclusion
+        include($url);
+        // ruleid: php-filter-inclusion
+        include_once($url);
+        // ruleid: php-filter-inclusion
+        require($url);
+        // ruleid: php-filter-inclusion
+        require_once($url);
+
+        // ruleid: php-filter-inclusion
+        include("a.php");
+        // ruleid: php-filter-inclusion
+        include_once("a.php");
+        // ruleid: php-filter-inclusion
+        require("a.php");
+        // ruleid: php-filter-inclusion
+        require_once("a.php");
+
+        // ok: php-filter-inclusion
+        includeo($url);
+    }
+}
